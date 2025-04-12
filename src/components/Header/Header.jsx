62c0ee4f6cd4ab1aa6e97f9a4c97e38fef732050
Triniti Nguyen <112030993+triniti-n@users.noreply.github.com>
2025-04-12 03:48:08 -0700

    Create Header.jsx

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import './Header.css'
+
+export default function header() {
+    return(
+        <nav className="Header">
+            <h1 className="game-title">Wordle Rivals</h1>
+        </nav>
+    )
+}
